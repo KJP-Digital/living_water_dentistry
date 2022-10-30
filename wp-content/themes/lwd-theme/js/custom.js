b5f80ae3f97cdf6dbdb3f1e75e1ae7a61d102ae2
KJP-Digital <115533119+KJP-Digital@users.noreply.github.com>
2022-10-30 13:17:37 +0530

    Add files via upload

--- wp-content/themes/lwd-theme/js/custom.js
+++ wp-content/themes/lwd-theme/js/custom.js
@@ -1,1 +1,20 @@
+// NAVIGATION - TAB/MOBILE VIEW
+$('.nav-menu-icon').click(function(){
+    $("html").toggleClass("menu-open");
+});
+$('.menu-open .nav-menu-icon:before').click(function(){
+    $("html").removeClass("menu-open");
+});
+$('.close-ico-mob').click(function(){
+    $("html").removeClass("menu-open");
+});
 
+//FOOTER RESPONSIVE EXPAND COLLAPSE JS
+$(".panelheading").click(function(){
+    var $ans = $(this).next(".panel-content");
+    var $ico = $(this);
+    $ans.slideToggle(500);
+    $ico.toggleClass("show");
+    $(".panel-content").not($ans).slideUp(500);
+    $(".panelheading").not($ico).removeClass("show");
+});
